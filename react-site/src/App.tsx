dea7d27a95f6b4b115294482144b5c7deb8ef0f7
Lucas V. <lksvn@outlook.com>
2026-08-25 12:03:13 -0300

    feat: scaffold react typscrit site

---
+++ react-site/src/App.tsx
@@ -0,0 +1,24 @@
+function App() {
+	return (
+		<>
+			<header>
+				<h1>Hello there!</h1>
+			</header>
+			<main>
+				<section>
+					<h2>About Me</h2>
+					<p>I'm a <strong>Web Developer</strong> based in <strong>Brazil</strong>, building and evolving <strong>commercial web applications</strong> and <strong>SaaS</strong> products since 2009.</p>
+				</section>
+				<section>
+					<h2>Projects</h2>
+					<p>Over the years I've worked on websites, landing pages, interfaces, branding projects and SaaS-related solutions.</p>
+				</section>
+			</main>
+			<footer>
+				Code since 1990
+			</footer>
+		</>
+	)
+}
+
+export default App
